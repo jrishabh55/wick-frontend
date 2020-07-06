8b1a783de602b304e22101258afa4d153e865f5a
Rishabh Jain <rishabh@codeation.io>
2020-07-06 15:34:47 +0530

    feat: google service

--- src/services/google.service.ts
+++ src/services/google.service.ts
@@ -1,20 +1,24 @@
-const scope = [
+export const scope = [
   'https://www.googleapis.com/auth/youtube',
   'https://www.googleapis.com/auth/youtube.force-ssl',
   'https://www.googleapis.com/auth/youtube.readonly',
   'https://www.googleapis.com/auth/youtubepartner-channel-audit',
-];
+].join(' ');
 
+export let GoogleAuth: gapi.auth2.GoogleAuth;
 
 export const initAuthClient = async () => {
   await (new Promise((resolve, reject) => gapi.load('client:auth2', resolve)));
+  gapi.client.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY as string);
+
   await gapi.client.init({
-      'apiKey': process.env.REACT_APP_GOOGLE_API_KEY,
-      'clientId': process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
-      'scope': scope.join(' '),
-      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+      scope,
+      apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+      clientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
   });
-  const GoogleAuth = gapi.auth2.getAuthInstance();
+
+  GoogleAuth = gapi.auth2.getAuthInstance();
   return GoogleAuth as Omit<gapi.auth2.GoogleAuth, "then">;
 }
 
