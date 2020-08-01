f7a169b2271f1ca8d4f7b47e5d1e4ca18210c70e
Rishabh Jain <rishabh@codeation.io>
2020-08-01 23:48:15 +0530

    spotify button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import './App.css';
 import { initAuthClient, scope } from './services/google.service';
+import { Button } from './components/Button';
 
 
 const loginWithGoogle =  async () => {
@@ -10,10 +11,10 @@ const loginWithGoogle =  async () => {
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isSpotifyLogin, setIsSpotifyLogin] = useState(false);
   const [user, setUser] = useState<gapi.auth2.GoogleUser>({} as gapi.auth2.GoogleUser);
-  const [data, setData] = useState({});
 
-  const login = useCallback(() => {
+  const loginWithGoogleFunc = useCallback(() => {
     loginWithGoogle()
       .then(res => {
         if (res.isSignedIn()) {
@@ -23,46 +24,30 @@ function App() {
       });
   }, []);
 
-  const fetchData = useCallback(() => {
-    // user.grantOfflineAccess(scope);
-    const authRes = user.getAuthResponse();
-    const { access_token, id_token } = authRes;
-    fetch('http://localhost:7000/auth/youtube/channels', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        access_token, id_token,
-      }),
-    }).then(res => res.json()).then(setData);
-  }, [user]);
+  const loginWithSpotify = useCallback(() => {
+    window.location.replace('http://localhost:50459/login/spotify');
+  }, []);
 
   return (
-    <div className="App">
-      {
+    <div className="App flex justify-center items-center h-screen flex-col">
+      {/* {
         !isLoggedIn && (
-          <button
-            className="rounded-lg border border-gray-400 hover:shadow-lg hover:border-gray-600 active:bg-blue-800 active:border-gray-600 shadow-md p-2 bg-blue-500 text-white"
-            onClick={login}
+          <Button
+            onClick={loginWithGoogleFunc}
           >
             Login with Google
-          </button>
+          </Button>
         )
-      }
+      } */}
       {
-        isLoggedIn && (
-          <button
-            className="rounded-lg border border-gray-400 hover:shadow-lg hover:border-gray-600 active:bg-blue-800 active:border-gray-600 shadow-md p-2 bg-blue-500 text-white"
-            onClick={fetchData}
+        !isSpotifyLogin && (
+          <Button
+            onClick={loginWithSpotify}
           >
-            Fetch Data
-          </button>
+            Login with Spotify.
+          </Button>
         )
       }
-      <pre>
-        {JSON.stringify(data, null, 4)}
-      </pre>
     </div>
   );
 }
